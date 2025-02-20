a9dd29cf6cc13f3bfa2a8eb2a65a8cc6f4cdd0e0
Kykylele <alfutovivan@gmail.com>
2025-02-20 18:43:26 +0300

    n3

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
@@ -14,15 +14,14 @@ def mission():
 @app.route('/promotion')
 def promotion():
     return '''Человечество вырастает из детства.<br><br>
-
 Человечеству мала одна планета.<br><br>
-
 Мы сделаем обитаемыми безжизненные пока планеты.<br><br>
-
 И начнем с Марса!<br><br>
-
 Присоединяйся!<br><br>'''
 
+@app.route('/image_mars')
+def mars_picture():
+    return render_template('image_mars.html')
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=8080)
